33d33184a91f05feed5817e4c66bf276872b5900
Patrik <kakerarecever567@gmail.com>
2021-11-23 19:09:46 +0100

    added log fuction fixed addtosettings check for empty string

--- Web Monitor/MainWindow.xaml.cs
+++ Web Monitor/MainWindow.xaml.cs
@@ -27,6 +27,8 @@ namespace Web_Monitor
             InitializeComponent();
             // Initialize the DB management 
             db_management = new Database_Management();
+
+            Log_Add("Program init ok");
         }
 
         /// <summary>
@@ -51,25 +53,36 @@ namespace Web_Monitor
             string path = addsettings_scrapepath.Text;
             string sitetoscrape = addsettings_scrapersite.Text;
 
+
+            if(string.IsNullOrEmpty(path) || string.IsNullOrEmpty(sitetoscrape))
+            {
+                Log_Add("Path or site to scrape is empty!");
+                // log it
+                return;
+            }
+
             int fetchtime = 60; // in seconds, default time that will be used for syncing
 
             Dictionary<string, string> dict =  Parsers.AddToSettings_OptionalValues(optionalvalues);
-            // We can pretty much ignore this as the user doesn't need to specify other args, so we will just show an message that args are not specified in the log
-            if(dict.Count == 0)
-            { 
-                Console.WriteLine("Either the argument is invalid, or the argument is not specified");
-            }
-            else
+            // We can pretty much ignore if the string is empty as the user doesn't need to specify other args
+            if (!string.IsNullOrEmpty(optionalvalues))
             {
+                // We need to check if the dict is not empty, if it's, abort the operation
+                if (dict.Count == 0)
+                { 
+                    MessageBox.Show("Invalid cmd for optional values, aborted.");
+                    return;
+                }
+
                 // We need to loop through the dictionary to get the values that we need
                 foreach (KeyValuePair<string, string> qva in dict)
                 {
-                    // Here we are going to get our values, this is going to be the best way do it, with case operator
+                    // Here we are going to get our values, this is going to be the best way do it, with switch operator
                     switch (qva.Key)
                     {
-                        // If any of those arguments is badly specified, it will cancel the adding function
+                        // Any error caused in the case function will result in the whole function stopping, to prevent more errors
                         case "fetchtime":
-                            if(!Int32.TryParse(qva.Value, out fetchtime)) // we are going to safely parse the string to int, in case it fails, we will display an error message
+                            if (!Int32.TryParse(qva.Value, out fetchtime)) // we are going to safely parse the string to int, in case it fails, we will display an error message
                             {
                                 MessageBox.Show("Unable to parse parameter 'fetchtime', argument cannot be string!");
                                 return;
@@ -78,14 +91,35 @@ namespace Web_Monitor
                     }
                 }
             }
+           
 
-            if(!db_management.CRON_AddJob("http://localhost:5000", "velkyPath"))
+
+            if(!db_management.CRON_AddJob(path, sitetoscrape))
             {
                 MessageBox.Show("An error has occured while adding the job into job list!");
             }
 
+            UI_CronData_ReloadData(); // Reload the data from db
             // debug messages
-            Console.WriteLine($"Fetch-time argument is: {fetchtime}");
+            Log_Add("Add to db ok");
         }
+
+
+        public void UI_CronData_ReloadData()
+        {
+
+        }
+
+
+        /// <summary>
+        /// Adds a msg to the LogBox textbox
+        /// </summary>
+        /// <param name="s">Message to add</param>
+        public void Log_Add(string s)
+        {
+            LogBox.Text += String.Format("{0} => {1}\r\n", DateTime.Now.ToString("HH:mm:ss"), s);
+        }
+
+
     }
 }
