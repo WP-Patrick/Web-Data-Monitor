8170a0e6df8d34af0a5c871de19c3bb2088ffbd6
Patrik <kakerarecever567@gmail.com>
2021-11-23 18:21:13 +0100

    Added UI AddToSettings parser Added parameter getting for AddToSettings add function

--- Web Monitor/MainWindow.xaml.cs
+++ Web Monitor/MainWindow.xaml.cs
@@ -36,7 +36,47 @@ namespace Web_Monitor
             addsettings_scrapersite.Text = "";
         }
 
+        /// <summary>
+        /// Save the job to the database and then show a message box that it was succesfull or the task failed to be added
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void UI_AddToSettings_AddButton(object sender, RoutedEventArgs e)
+        {
+            string optionalvalues = addsettings_optionalargs.Text;
+            string path = addsettings_scrapepath.Text;
+            string sitetoscrape = addsettings_scrapersite.Text;
 
+            int fetchtime = 60; // in seconds, default time that will be used for syncing
 
+            Dictionary<string, string> dict =  Parsers.AddToSettings_OptionalValues(optionalvalues);
+            // We can pretty much ignore this as the user doesn't need to specify other args, so we will just show an message that args are not specified in the log
+            if(dict.Count == 0)
+            { 
+                Console.WriteLine("Either the argument is invalid, or the argument is not specified");
+            }
+            else
+            {
+                // We need to loop through the dictionary to get the values that we need
+                foreach (KeyValuePair<string, string> qva in dict)
+                {
+                    // Here we are going to get our values, this is going to be the best way do it, with case operator
+                    switch (qva.Key)
+                    {
+                        // If any of those arguments is badly specified, it will cancel the adding function
+                        case "fetchtime":
+                            if(!Int32.TryParse(qva.Value, out fetchtime)) // we are going to safely parse the string to int, in case it fails, we will display an error message
+                            {
+                                MessageBox.Show("Unable to parse parameter 'fetchtime', argument cannot be string!");
+                                return;
+                            }
+                            break;
+                    }
+                }
+            }
+
+            // debug messages
+            Console.WriteLine($"Fetch-time argument is: {fetchtime}");
+        }
     }
 }
---
+++ Web Monitor/Parsers.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Web_Monitor
+{
+    public static class Parsers
+    {
+        /// <summary>
+        /// Converts the string to dictionary of parameters used for configuring the job
+        /// The string is splitted by ,
+        /// </summary>
+        /// <param name="ov"></param>
+        /// <returns></returns>
+        public static Dictionary<string, string> AddToSettings_OptionalValues(string ov)
+        {
+            Dictionary<string, string> toReturn = new Dictionary<string, string>();
+            // This will indicate that something is wrong and that we do not even bother checking the other conditions
+            // if = is not in the string, then the whole string with arguments is invalid
+            // We are also going to check wheter the string is not empty
+            if (!ov.Contains('=') || string.IsNullOrEmpty(ov))
+                return toReturn;
+            string[] ov_splitted = ov.Split(','); 
+            foreach(string s in ov_splitted)
+            {
+                // Now we need to add the data into dictionary
+                string[] s_splitted = s.Split('='); // temp variable
+                if (s_splitted.Length != 2)
+                    return toReturn;
+                toReturn.Add(s_splitted[0], s_splitted[1]); // if everything is ok, add it to the dict
+            }
+            return toReturn;
+        } 
+    }
+}
